b6409fb63972ba342fd82f228b56e6ddc049eb15
mahima051998 <36787931+mahima051998@users.noreply.github.com>
2023-04-15 19:28:11 -0400

    Update client.c

--- client.c
+++ client.c
@@ -11,24 +11,30 @@
 #define PORT 3000
 #define BUFFER_SIZE 1024
 
+// This function takes a string input as a command and processes it, returning the result as a string.
 char* process_command(char* input) {
+    // Allocate memory for the result string and create an array to store command arguments.
     char* result = (char*)malloc(sizeof(char) * 1024);
     char* args[2] = {NULL, NULL};
 
+    // If the command matches the "findfile" pattern, find the file and store the result in the result string.
     if (sscanf(input, "findfile %ms", &args[0]) == 1) {
-        snprintf(result, 1024, "echo -e $(find ~/ -maxdepth 1 -type f -name %s -exec stat -c '%%s\\t%%w\\t%%n' {} \\; | head -n 1)", args[0]);
+        sprintf(result, 1024, "output=$(find ~/ -maxdepth 1 -type f -name %s -exec stat -c '%%s\\t%%w\\t%%n' {} \\; | head -n 1); if [ -z \"$output\" ]; then echo \"File not found\"; else echo -e \"$output\"; fi", res[0]);
         free(args[0]);
     } 
+    // If the command matches the "sgetfiles" pattern, get files based on their size range and store the result in the result string.
     else if (sscanf(input, "sgetfiles %ms %ms%*c", &args[0], &args[1]) == 2) {
         snprintf(result, 1024, "zip temp.tar.gz $(find ~/ -maxdepth 1 -type f -size +%sc -size -%sc)", args[0], args[1]);
         free(args[0]);
         free(args[1]);
     } 
+    // If the command matches the "dgetfiles" pattern, get files modified between two dates and store the result in the result string.
     else if (sscanf(input, "dgetfiles %ms %ms%*c", &args[0], &args[1]) == 2) {
         snprintf(result, 1024, "zip temp.tar.gz $(find ~/ -maxdepth 1 -type f -newermt \"%s\" ! -newermt \"%s\")", args[0], args[1]);
         free(args[0]);
         free(args[1]);
     } 
+    // If the command matches the "getfiles" pattern, get files based on a list of file names and store the result in the result string.
     else if (sscanf(input, "getfiles %[^\n]", result) == 1) {
         char files[1024] = "";
         char* token = strtok(result, " ");
@@ -45,29 +51,42 @@ char* process_command(char* input) {
         }
         sprintf(result, "zip temp.tar.gz $(find ~/ -maxdepth 1 -type f %s)", files);
     } 
+    // If the command matches the "gettargz" pattern, get files based on their extension and store the result in the result string.
     else if (sscanf(input, "gettargz %[^\n]", result) == 1) {
         char files[1024] = "";
+        // Use strtok to tokenize the "result" string by space
         char* token = strtok(result, " ");
         int count = 0;
+        // Loop through each token
         while (token != NULL) {
+            // Initialize a temporary character array to store the current file name
             char tmp[256];
+            // If this is not the first file name, add "-o" to the "files" array
             if (count > 0) {
                 strcat(files, "-o ");
             }
+            // Use snprintf to create the command for finding files with the current extension
             snprintf(tmp, 256, "-iname \"*.%s\" ", token);
+            // Append the current command to the "files" array
             strcat(files, tmp);
             count++;
+             // Move on to the next token
             token = strtok(NULL, " ");
         }
+        // Use sprintf to create the command for zipping files with the specified extensions
         sprintf(result, "zip temp.tar.gz $(find ~/ -maxdepth 1 -type f %s)", files);
     } 
+    // Check if the input is the "quit" command
     else if (strcmp(input, "quit") == 0) {
+        // Exit the program with status code 0
         exit(0);
     } 
+    // If the input is not a recognized command
     else {
+        // Use snprintf to create an error message
         snprintf(result, 1024, "Invalid Command");
     }
-
+    // Return the result string
     return result;
 }
 
